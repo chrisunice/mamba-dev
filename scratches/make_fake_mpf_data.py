9b3a9e332785e7ddfb23db5f7645ed26b3a9678d
ChrisUnice <chrisunice@gmail.com>
2022-12-31 12:22:56 -0800

    making fake data for multiple platforms

--- scratches/make_fake_mpf_data.py
+++ scratches/make_fake_mpf_data.py
@@ -39,34 +39,36 @@ def pol_gen():
 
 
 if __name__ == '__main__':
-    missions = {}
 
-    # Define 50 missions to generate random data for ...
-    num_missions = 50
-    for msn_num in range(1, num_missions+1):
+    for platform in ['A-10', 'F-22', 'F-35', 'B-1', 'B-2']:
+        missions = {}
 
-        # Index the mission
-        missions[msn_num] = {}
+        # Define 50 missions to generate random data for ...
+        num_missions = 50
+        for msn_num in range(1, num_missions+1):
 
-        # Fake UMI
-        missions[msn_num]['umi'] = next(umi_gen())
+            # Index the mission
+            missions[msn_num] = {}
 
-        # Fake configuration
-        missions[msn_num]['config'] = next(av_config_gen())
+            # Fake UMI
+            missions[msn_num]['umi'] = next(umi_gen())
 
-        # Fake sub configuration
-        missions[msn_num]['sub_config'] = next(av_sub_config_gen())
+            # Fake configuration
+            missions[msn_num]['config'] = next(av_config_gen())
 
-        # Fake vector groups
-        vectors = []
-        num_vectors = random.randint(10, 100)
-        for _ in range(num_vectors):
-            vector = f"{next(freq_gen())} MHz | {next(pol_gen())[0]}-pol"
-            if vector not in vectors:
-                vectors.append(vector)
-        missions[msn_num]['vectors'] = sorted(vectors, key=lambda x: int(x.split(' ')[0]))
+            # Fake sub configuration
+            missions[msn_num]['sub_config'] = next(av_sub_config_gen())
 
-    # Save to json object
-    path_to_test_assets = 'C:\\Mamba\\test_assets'
-    with open(f"{path_to_test_assets}\\fake_metadata.json", mode='w+') as f:
-        json.dump(missions, f)
+            # Fake vector groups
+            vectors = []
+            num_vectors = random.randint(10, 100)
+            for _ in range(num_vectors):
+                vector = f"{next(freq_gen())} MHz | {next(pol_gen())[0]}-pol"
+                if vector not in vectors:
+                    vectors.append(vector)
+            missions[msn_num]['vectors'] = sorted(vectors, key=lambda x: int(x.split(' ')[0]))
+
+        # Save to json object
+        path_to_test_assets = 'C:\\Mamba\\test_assets'
+        with open(f"{path_to_test_assets}\\{platform.lower()}-mongodb.json", mode='w+') as f:
+            json.dump(missions, f)
