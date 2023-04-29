9b723bb0996d030de49d83ef908554fcb2fbbb68
ChrisUnice <chrisunice@gmail.com>
2023-04-28 17:11:17 -0700

    added authentication

--- mamba_dev/index.py
+++ mamba_dev/index.py
@@ -1,4 +1,5 @@
 # from waitress import serve
+import dash_auth
 
 import mamba_ui as mui
 from mamba_dev import config
@@ -9,11 +10,18 @@ from mamba_dev.settings import *
 
 if __name__ == "__main__":
 
+    # App Authentication
+    dash_auth.BasicAuth(
+        app=mui.app,
+        username_password_list=dict(config['login'])
+    )
+
+
     mui.app.layout = mui.serve_layout()
     run_kwargs = dict(
         debug=bool(int(config['default']['debug_mode'])),
         host=config['default']['ip_address'],
-        port=int(config['default']['port'])
+        port=int(config['default']['port']),
     )
     if run_kwargs.get('debug'):
         run_kwargs.pop('host')
