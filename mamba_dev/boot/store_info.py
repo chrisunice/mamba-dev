4d577ba823ebda29fdfc4d0f8a9ac0d0e8868167
ChrisUnice <chrisunice@gmail.com>
2023-04-28 18:05:40 -0700

    callback to create a session folder for the client per browswer session

--- mamba_dev/boot/store_info.py
+++ mamba_dev/boot/store_info.py
@@ -1,22 +1,41 @@
 import os
+import json
 from flask import request
-from dash_extensions.enrich import Trigger
+from datetime import datetime
+from dash_extensions.enrich import Trigger, Output, State
 
 import mamba_ui as mui
 from mamba_dev import config
 
 
 @mui.app.callback(
-    Trigger('dash-layout', 'children')
+    Output('session-store', 'data'),
+    Trigger('dash-layout', 'children'),
+    State('session-store', 'data')
 )
-def store_serverside():
-    # Make client folder
-    root_dir = config['default']['root_dir']
-    username = request.authorization['username']
-    client_folder = f"{root_dir}/clients/{username}"
+def store_session_serverside(session_data):
+    """ Store a session folder for the client on the server that is tied to the life of the browser """
+    if session_data is None:
+        # Make client folder
+        root_dir = config['default']['root_dir']
+        username = request.authorization['username']
+        client_folder = f"{root_dir}/clients/{username}"
 
-    try:
-        os.mkdir(client_folder)
-    except FileExistsError:
-        pass
-    
+        # Make a client folder if one doesn't already exist
+        try:
+            os.mkdir(client_folder)
+        except FileExistsError:
+            pass
+
+        # Make a session folder for the client
+        dt = datetime.utcnow()
+        string_time = dt.strftime("%Y-%m-%d_%H.%M.%SZ")
+        session_folder = f"{client_folder}/{string_time}"
+        try:
+            os.mkdir(session_folder)
+        except:
+            pass
+
+        return json.dumps(dict(_dummy=None))
+    else:
+        return session_data
