175e4f49ea343ba10bc4bb841cee902733905d8b
ChrisUnice <chrisunice@gmail.com>
2023-04-28 17:11:55 -0700

    building requirements.txt file nicely

--- update.ps1
+++ update.ps1
@@ -1,8 +1,14 @@
+# Update pip and grab all requirements
 python -m pip install --upgrade pip
 pip install -r .\requirements.txt
 
+# Update requirements file
+pip-chill > .\requirements.txt
+
+# Reinstall lodat
 pip uninstall lodat -y
 pip install lodat --no-index --find-links=U:\PyPI
 
+# Reinstall mamba user interface
 pip uninstall mamba_ui -y
 pip install mamba_ui --no-index --find-links=U:\PyPI
