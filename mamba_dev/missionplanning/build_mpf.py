5bdca5654e7f6867a236c47742ac78f7ba798011
ChrisUnice <chrisunice@gmail.com>
2023-09-12 14:43:40 -0700

    simulating build mpf logic for work

--- mamba_dev/missionplanning/build_mpf.py
+++ mamba_dev/missionplanning/build_mpf.py
@@ -1,10 +1,10 @@
 import dash
 import json
 import time
-import numpy as np
 import mamba_ui as mui
+import numpy as np
 from dash.exceptions import PreventUpdate
-from dash_extensions.enrich import Input, Output
+from dash_extensions.enrich import Input, Output, State
 
 from mamba_dev.missionplanning import log_queue
 
@@ -15,35 +15,46 @@ def _fake_mpf_builder():
     return None
 
 
-@dash.callback(
-    outputs=Output('mission-planning-download-modal', 'is_open'),
-    inputs=[
-        Input('mission-planning-page-submit-button', 'n_clicks'),
-        Input('mission-planning-store', 'data')
-    ],
-    background=True,
-    cancel=[Input('mission-planning-page', 'children')]
+@mui.app.callback(
+    Output('mission-planning-download-modal', 'is_open'),
+    Input('mission-planning-page-submit-button', 'n_clicks'),
+    State('mission-planning-download-modal', 'is_open')
 )
-def build_mpf(submit_click, data):
-    if submit_click is None or data is None:
+def open_modal(submit_click, modal_open):
+    if submit_click is None:
         raise PreventUpdate
 
-    data = json.loads(data)
-    inputs = data['inputs']
+    return not modal_open
 
-    # Things to iterate over
-    vectors = inputs['vectors']
-    looks = np.arange(inputs['look_min'], inputs['look_max'], inputs['look_width'])
-    deprs = np.arange(inputs['depr_min'], inputs['depr_max'], inputs['depr_width'])
-
-    # Iterate and store in queue
-    for vg in vectors:
-        for lk in looks:
-            for dp in deprs:
-                message = f"Working on {vg}, Look: {lk}, Depr: {dp}"
-                log_queue.put(message)
-                print(f'Just added \n{message}')
-                _fake_mpf_builder()
-
-    # Pop modal
-    return True
+# @dash.callback(
+#     outputs=Output('mission-planning-download-modal', 'is_open'),
+#     inputs=[
+#         Input('mission-planning-page-submit-button', 'n_clicks'),
+#         Input('mission-planning-store', 'data')
+#     ],
+#     background=True,
+#     cancel=[Input('mission-planning-page', 'children')]
+# )
+# def build_mpf(submit_click, data):
+#     if submit_click is None or data is None:
+#         raise PreventUpdate
+#
+#     data = json.loads(data)
+#     inputs = data['inputs']
+#
+#     # Things to iterate over
+#     vectors = inputs['vectors']
+#     looks = np.arange(inputs['look_min'], inputs['look_max'], inputs['look_width'])
+#     deprs = np.arange(inputs['depr_min'], inputs['depr_max'], inputs['depr_width'])
+#
+#     # Iterate and store in queue
+#     for vg in vectors:
+#         for lk in looks:
+#             for dp in deprs:
+#                 message = f"Working on {vg}, Look: {lk}, Depr: {dp}"
+#                 log_queue.put(message)
+#                 print(f'Just added \n{message}')
+#                 _fake_mpf_builder()
+#
+#     # Pop modal
+#     return True
