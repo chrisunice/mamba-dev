5e3c49fb3d5853c5ea36b222d5ceca90c5f79019
ChrisUnice <chrisunice@gmail.com>
2023-09-12 19:18:11 -0700

    updated monitor imagery to work in a thread rather than in a separate process because its unsafe to spawn a process within the Dash app callback process

--- mamba_dev/settings/monitor_imagery.py
+++ mamba_dev/settings/monitor_imagery.py
@@ -1,6 +1,7 @@
 import time
 from flask import request
 import multiprocessing as mp
+import threading
 from dash_extensions.enrich import Input, Trigger
 
 import mamba_ui as mui
@@ -8,45 +9,63 @@ import mamba_ui as mui
 # Global variables
 # Not good practice to have global variables that could be edited by any client that connects to the server
 # However, these variables are used to track the state of the imagery process so that clients don't duplicate work
-IMAGERY_PROCESS_RUNNING = False
-IMAGERY_PROCESS_NAME = 'imagery'
+IMAGERY_THREAD_RUNNING = False
+SHUTDOWN_EVENT = threading.Event()
 
 
 def worker():
     """ Dummy function """
-    while True:
-        # print(f'\tRunning on process {mp.current_process().pid}')
-        # time.sleep(0.5)
+    global IMAGERY_THREAD_RUNNING
+
+    while not SHUTDOWN_EVENT.is_set():
+        # do work
+        print(f'\tImagery thread is doing work son')
+        time.sleep(0.5)
         pass
+    # Set the thread flag to False
+    IMAGERY_THREAD_RUNNING = False
+    return None
 
 
-def _get_imagery_process() -> mp.Process | None:
-    """ Get the imaging process if one exists in the parent process """
-    global IMAGERY_PROCESS_NAME
+# def _get_imagery_process() -> mp.Process | None:
+#     """ Get the imaging process if one exists in the parent process """
+#     global IMAGERY_THREAD_NAME
+#
+#     child_processes = mp.active_children()
+#     if bool(child_processes):
+#         # Find the imagery process
+#         for child in child_processes:
+#             if child.name == IMAGERY_THREAD_NAME:
+#                 process = child
+#     else:
+#         process = None
+#     return process
 
-    child_processes = mp.active_children()
-    if bool(child_processes):
-        # Find the imagery process
-        for child in child_processes:
-            if child.name == IMAGERY_PROCESS_NAME:
-                process = child
-    else:
-        process = None
-    return process
+
+def start_thread():
+    global IMAGERY_THREAD_RUNNING, SHUTDOWN_EVENT
+
+    # Make sure there isn't any prior shut down events
+    SHUTDOWN_EVENT.clear()
+
+    # Spawn and start a new thread
+    thread = threading.Thread(target=worker, daemon=True)
+    thread.start()
+
+    # Flip the thread bit
+    IMAGERY_THREAD_RUNNING = True
+    return None
 
 
 @mui.app.callback(
     Trigger('dash-layout', 'children'),
 )
 def init_monitoring():
-    """ Starts an imagery process on application load if one is not already running """
-    global IMAGERY_PROCESS_RUNNING
-
-    if not IMAGERY_PROCESS_RUNNING:
-        process = mp.Process(name=IMAGERY_PROCESS_NAME, target=worker)
-        process.start()
-        IMAGERY_PROCESS_RUNNING = True
-        print(f'{process} has started by {request.remote_addr}')
+    """ Start an imagery thread on application load if one is not already running """
+    global IMAGERY_THREAD_RUNNING
+    if not IMAGERY_THREAD_RUNNING:
+        start_thread()
+        print(f'Imagery thread has been started by {request.remote_addr}')
 
 
 @mui.app.callback(
@@ -56,31 +75,26 @@ def init_monitoring():
 def monitor_imagery(switch_on):
     """ Handles starting and stopping imagery process based on switch inputs """
 
-    global IMAGERY_PROCESS_NAME, IMAGERY_PROCESS_RUNNING
+    global IMAGERY_THREAD_RUNNING
 
     # Scenario 1 - switch is on and image process is already running
-    if switch_on and IMAGERY_PROCESS_RUNNING:
-        # do nothing
-        pass
+    if switch_on and IMAGERY_THREAD_RUNNING:
+        pass        # do nothing
     # Scenario 2 - switch is off and image process is running
-    elif not switch_on and IMAGERY_PROCESS_RUNNING:
-        # kill it
-        process = _get_imagery_process()
-        process.terminate()
-        IMAGERY_PROCESS_RUNNING = False
-        print(f'{process} has stopped by {request.remote_addr}')
+    elif not switch_on and IMAGERY_THREAD_RUNNING:
+        # trigger shutdown event
+        SHUTDOWN_EVENT.set()
+        print(f'Imagery thread has been stopped by {request.remote_addr}')
     # Scenario 3 - switch is on and image process has stopped
-    elif switch_on and not IMAGERY_PROCESS_RUNNING:
+    elif switch_on and not IMAGERY_THREAD_RUNNING:
         # start it
-        process = mp.Process(name=IMAGERY_PROCESS_NAME, target=worker)
-        process.start()
-        IMAGERY_PROCESS_RUNNING = True
-        print(f'{process} has started by {request.remote_addr}')
+        start_thread()
+        print(f'Imagery thread has been started by {request.remote_addr}')
     # Scenario 4 - switch is off and image process has stopped
-    elif not switch_on and not IMAGERY_PROCESS_RUNNING:
+    elif not switch_on and not IMAGERY_THREAD_RUNNING:
         # do nothing
         pass
     else:
         raise ValueError(
-            f'Unhandled scenario with switch state {switch_on} and process state {IMAGERY_PROCESS_RUNNING}'
+            f'Unhandled scenario with switch state {switch_on} and process state {IMAGERY_THREAD_RUNNING}'
         )
