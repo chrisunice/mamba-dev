208fdcebf61686cbe22f110fffa384b9245d5320
ChrisUnice <chrisunice@gmail.com>
2023-09-16 14:29:07 -0700

    stopped the imagery monitor task from running while I work on the MPF code

--- mamba_dev/settings/__init__.py
+++ mamba_dev/settings/__init__.py
@@ -1,2 +1,3 @@
-from .monitor_imagery import init_monitoring, monitor_imagery
+# from .monitor_imagery import init_monitoring, monitor_imagery
+from .monitor_imagery import monitor_imagery
 from .toggle_settings import toggle_settings_window
