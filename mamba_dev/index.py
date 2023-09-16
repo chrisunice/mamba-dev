bd91bd91170ad2e4cb8bd3b207eb2d8603639cd9
ChrisUnice <chrisunice@gmail.com>
2023-09-16 14:30:14 -0700

    added logging instead of print statements

--- mamba_dev/index.py
+++ mamba_dev/index.py
@@ -1,27 +1,23 @@
 # Note these are for debugging the processes
 import os
 import psutil
-
-# from waitress import serve
 import dash_auth
+# from waitress import serve
 
 import mamba_ui as mui
-
 from mamba_dev import config
+from mamba_dev import logger
 
 from mamba_dev.boot import *
 from mamba_dev.home import *
 from mamba_dev.settings import *
-
 from mamba_dev.sandbox import *
 from mamba_dev.missionplanning import *
 
 
 if __name__ == "__main__":
 
-    current_pid = os.getpid()
-    parent_pid = psutil.Process(current_pid).ppid()
-    print(f"The Dash application is running in process: {current_pid} and its parent process is: {parent_pid}")
+    logger.debug('Starting mamba...')
 
     # App Authentication
     dash_auth.BasicAuth(
@@ -35,6 +31,6 @@ if __name__ == "__main__":
         host=config['default']['ip_address'],
         port=int(config['default']['port']),
     )
-    if run_kwargs.get('debug'):
-        run_kwargs.pop('host')
+    # if run_kwargs.get('debug'):
+    #     run_kwargs.pop('host')
     mui.app.run(**run_kwargs)
