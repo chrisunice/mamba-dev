95b243b2b8963ec98acddc09589bef280bf5e5e7
ChrisUnice <chrisunice@gmail.com>
2023-09-28 20:32:12 -0700

    trying to get disable tab focus to work

--- mamba_dev/missionplanning/__init__.py
+++ mamba_dev/missionplanning/__init__.py
@@ -1,7 +1,3 @@
-from queue import Queue
-
-log_queue = Queue()
-
 from .populate_platform import populate_platform, display_selection, force_one
 from .populate_av_config import populate_av_config, display_selection
 from .populate_av_sub_config import populate_av_sub_config, display_selection
@@ -10,15 +6,14 @@ from .populate_vectors import populate_vectors, display_selection, select_all_or
 from .handle_platform_switch import handle_platform_switch
 from .update_look_range import update_look_range
 from .update_depr_range import update_depr_range
+# from .update_depr_range import update_min_depr, update_max_depr
 from .populate_metrics import populate_metrics, display_selection
 from .show_hide_percentile import show_hide_percentile
 from .handle_inputs import handle_inputs
 from .reset_all import reset_all
 from .init_console import init_console
 # from .update_console import update_console
-from .clear_log import clear_log
+# from .clear_log import clear_log
 
 from .build_mpf_background import build_mpf
 from .handle_mpf_download import handle_download_button, handle_close_button
-
-from .disable_slider_tab_focus import disableTabFocus
