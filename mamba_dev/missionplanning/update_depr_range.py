5d3746647aa6818d90b7c2df52e13cf982d238fe
ChrisUnice <chrisunice@gmail.com>
2023-09-28 20:31:21 -0700

    messing around with some code to handle each side of the slider individually. I think this may be unnecessary

--- mamba_dev/missionplanning/update_depr_range.py
+++ mamba_dev/missionplanning/update_depr_range.py
@@ -1,6 +1,6 @@
 import mamba_ui as mui
 from dash.exceptions import PreventUpdate
-from dash_extensions.enrich import CycleBreakerInput, Output, ctx
+from dash_extensions.enrich import CycleBreakerInput, Output, ctx, State
 
 
 @mui.app.callback(
@@ -27,4 +27,47 @@ def update_depr_range(
     return slider_value, min_value, max_value
 
 
-
+# @mui.app.callback(
+#     Output('depression-range-slider', 'value'),
+#     Output('depression-range-min-input', 'value'),
+#     CycleBreakerInput('depression-range-slider', 'value'),
+#     CycleBreakerInput('depression-range-min-input', 'value'),
+#     State('depression-range-slider', 'value'),
+#     prevent_initial_call=True
+# )
+# def update_min_depr(slider_value, min_value, old_slider_value):
+#     # Do nothing on page load
+#     if all([param is None for param in locals().values()]):
+#         raise PreventUpdate
+#
+#     if 'input' in ctx.triggered_id:
+#         # Input was triggered
+#         if old_slider_value is None:
+#             old_slider_value = [None, None]
+#         slider_value = [min_value, old_slider_value[-1]]
+#     else:
+#         # Slider was triggered
+#         min_value = slider_value[0]
+#     return slider_value, min_value
+#
+#
+# @mui.app.callback(
+#     Output('depression-range-slider', 'value'),
+#     Output('depression-range-max-input', 'value'),
+#     CycleBreakerInput('depression-range-slider', 'value'),
+#     CycleBreakerInput('depression-range-max-input', 'value'),
+#     State('depression-range-slider', 'value'),
+#     prevent_initial_call=True
+# )
+# def update_max_depr(slider_value, max_value, old_slider_value):
+#     # Do nothing on page load
+#     if all([param is None for param in locals().values()]):
+#         raise PreventUpdate
+#
+#     if 'input' in ctx.triggered_id:
+#         # Input was triggered
+#         slider_value = [old_slider_value[0], max_value]
+#     else:
+#         # Slider was triggered
+#         max_value = slider_value[-1]
+#     return slider_value, max_value
